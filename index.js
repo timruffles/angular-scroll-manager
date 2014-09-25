13f45a98a244e6054234b1c981cf7f383e2bc20c
Tim Ruffles <oi@truffles.me.uk>
2014-09-25 22:23:31 +0100

    testing

--- index.js
+++ index.js
@@ -13,10 +13,11 @@ angular.module("scroll-manager", [])
         var currentTargets = {};
         var el = $el[0];
 
-        attrs.$observe("scrollTriggers", function(newTriggersString, oldTriggers) {
-          var newTriggers = scope.$eval(newTriggersString);
-          
-          if(typeof newTriggers != "object") {
+        scope.$watch(attrs.scrollTriggers, triggerWatcher, true);
+
+        function triggerWatcher(newTriggers) {
+        
+          if(!newTriggers || typeof newTriggers != "object") {
             return;
           }
 
@@ -24,20 +25,23 @@ angular.module("scroll-manager", [])
 
           events.forEach(function(event) {
             if(newTriggers[event]) {
-              if(!(event in currentTargets) ) {
-                scope.$on(event, handler);
+              if(!(event in currentTargets)) {
+                currentTargets[event] = { deregister: scope.$on(event, handler) };
               }
-              currentTargets[event] = newTriggers[event];
-            } else {
-              scope.$off(event, handler);
+              currentTargets[event].selector = newTriggers[event];
+            } else if(currentTargets[event]) {
+              currentTargets[event].deregister();
               delete currentTargets[event];
             }
           });
 
-        }, true);
+        }
 
         function handler(event) {
-          scrollManager.scroll( el.querySelector(currentTargets[event.name]) );
+          var match = el.querySelector(currentTargets[event.name].selector);
+          if(match) {
+            scrollManager.scroll(match);
+          }
         }
 
       },
--- test/scroll_manager_test.js
+++ test/scroll_manager_test.js
@@ -1,9 +1,10 @@
 
 describe("scroll-manager", function() {
 
-  var scrollSpy = sinon.spy();
+  var scrollSpy;
 
   beforeEach(module("scroll-manager", function($provide, $controllerProvider) {
+
     $controllerProvider.register('emitter', function($scope, $rootScope) {
 
       $rootScope.fireComplete = function() {
@@ -12,22 +13,26 @@ describe("scroll-manager", function() {
 
     });
 
-    $provide.value("scrollManagerScrollTo", scrollSpy); 
+    scrollSpy = sinon.spy();
+    $provide.value("scrollManager", { scroll: scrollSpy }); 
   }));
 
-  describe("simple case", function() {
+  describe("single event", function() {
     var el;
     var rootScope;
 
     beforeEach(inject(function($compile, $rootScope) {
       scope = $rootScope.$new();
+      scope.selectedItem = { id: "a" };
+      scope.enabled = true;
 
       el = $compile(heredoc(function() {/*
         <div scroll-triggers="{
-            'complete': '[data-scroll-id=a]' 
+            'complete': enabled ? '[data-scroll-id=' + selectedItem.id +']' : false 
           }">
           <div ng-controller='emitter'></div>
           <div data-scroll-id=a>Hello</div>
+          <div data-scroll-id=b>Bye</div>
         </div>
       */}))(scope);
 
@@ -36,11 +41,33 @@ describe("scroll-manager", function() {
 
     it("scrolls to element after matching event fires", function() {
       scope.fireComplete();
-      var target = el[0].querySelector("[data-scroll-id]");
+      var target = el[0].querySelector("[data-scroll-id=a]");
       chai.assert(target, "no target");
 
       sinon.assert.calledOnce(scrollSpy);
       sinon.assert.calledWith(scrollSpy, target);
+    });
+
+    it("keeps the selector up to date ", function() {
+      scope.selectedItem.id = "b";
+      scope.$apply();
+
+      scope.fireComplete();
+
+      var target = el[0].querySelector("[data-scroll-id=b]");
+      chai.assert(target, "no target");
+
+      sinon.assert.calledOnce(scrollSpy);
+      sinon.assert.calledWith(scrollSpy, target);
+    });
+
+    it("falsey disables previous listeners", function() {
+      scope.enabled = false;
+      scope.$apply();
+
+      scope.fireComplete();
+
+      sinon.assert.notCalled(scrollSpy);
     });
 
   });
